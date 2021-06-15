877e5e6b435351d14ec6fdc203ff72e4e0c42886
Mike-Chaban <cmisa695@gmail.com>
2021-06-15 21:05:58 +0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Styles/main.css
    #	index.php
    #	siteItems/header.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ require_once(ROOT.'\config\connect.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Bootstrap/css/all.css">
     <link rel="stylesheet" href="Styles/main.css">
     <link rel="stylesheet" href="Styles/mainPage.css">
     <link rel="stylesheet" href="Styles/header.css">
