bc2ca3886b2cf23668288afd3978cfac0a5990ac
nickiway <nickiwayua@gmail.com>
2021-06-15 22:07:00 +0300

    15/06/2021 22:06 Nick

--- functions/getGoods.php
+++ functions/getGoods.php
@@ -1,6 +1,7 @@
 <?php
 $getItems = mysqli_query($connect, "SELECT * FROM goods");
 while($row = mysqli_fetch_assoc($getItems)){
+    // Checking whether we have sales
     if ($row['Sale'] == 0) {
         $displaySale = "none";
         $textDecoration = "none";
@@ -9,13 +10,12 @@ while($row = mysqli_fetch_assoc($getItems)){
         $displaySale = "block";
         $textDecoration = "line-through";
     };
+    // Checking whether the items in stock
     if ($row['In stock'] == 1) {
-        $inStock = 'display:none';
-        $hideButton = 'display:block';
     } else {
-        $inStock = 'display:block';
-        $hideButton = 'display:none';
+        continue;
     }
+    // creating new cards
     echo 
     "<div class='cards'>
         <div style = 'display : ".$displaySale."' class='cards__sale font-bold setShadow'>Скидка — ".$percent."%</div>
@@ -24,7 +24,6 @@ while($row = mysqli_fetch_assoc($getItems)){
 
         <div class='cards__information'>
             <div class='cards__name toCenter font-bold'>".$row['Name']."</div>
-            <hr size = '2px' color = '#000'>
             <div class='cards__details'>
                 <div class='cards__description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, odio!</div>
                 <div class='toCenter'>
@@ -32,8 +31,8 @@ while($row = mysqli_fetch_assoc($getItems)){
                     <div class='cards__cost font-bold' style = 'display : ".$displaySale."'>".$row['Sale']." гривен</div>
                 </div>
             </div>
-            <button style = '".$hideButton." 'class = 'font-bold cards__button setShadow'>В корзину</button>
-            <div class='toCenter cards__stock font-bold' style = '".$inStock."'>Нет в наличии</div>
+
+            <button class = 'font-bold cards__button setShadow'>В корзину</button>
         </div>
     
     </div>";
