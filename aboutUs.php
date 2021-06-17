b9158fed086ed5ead2335bddec15d6516adf2011
Mike-Chaban <cmisa695@gmail.com>
2021-06-17 14:49:32 +0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Styles/main.css
    #	index.php
    #	siteItems/header.php

--- aboutUs.php
+++ aboutUs.php
@@ -1,5 +1,5 @@
 <?php
-// common settings 
+// common settings
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 // connecting files
@@ -14,27 +14,26 @@ require_once(ROOT.'\config\connect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Bootstrap/css/all.css">
     <link rel="stylesheet" href="Styles/medias.css">
-    <link rel="stylesheet" href="Styles/main.css">
-    <link rel="stylesheet" href="Styles/mainPage.css">
 <!--    <link rel="stylesheet" href="Styles/footer.css">-->
     <link rel="stylesheet" href="Styles/header.css">
     <link rel="stylesheet" href="Styles/preloader.css">
+    <link rel="stylesheet" href="Styles/aboutUs.css">
     <title>Добро пожаловать!</title>
 </head>
 <body>
     <? require_once(ROOT.'\siteItems\preloader.php'); ?>
     <!-- Header -->
-    <? require_once(ROOT.'\siteItems\header.php'); ?> 
+    <? require_once(ROOT.'\siteItems\header.php'); ?>
     <!-- Body -->
-    <div class="main">
-        <div class="toCenter">
-            <div class="main__namer font-bold">Товары на Ваш выбор</div>
+    <div class="box">
+        <div class="ourimg">
+            <img src="images/Img1.jpg" alt="About Us" width="100%">
+            <img src="images/Img1.jpg" alt="About Us">
         </div>
-        <div class="toCenter">
-            <div class="main__items toCenter">
-                <? require_once(ROOT.'\functions\getGoods.php');?>
-            </div>
+        <div>
+
         </div>
+
     </div>
     <!-- Footer -->
 </body>
