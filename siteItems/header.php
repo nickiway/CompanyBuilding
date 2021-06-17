117b98b9cf2153f32701fc73c5623101e4c9ea61
Mike-Chaban <cmisa695@gmail.com>
2021-06-17 13:03:11 +0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Styles/main.css
    #	index.php
    #	siteItems/header.php

--- siteItems/header.php
+++ siteItems/header.php
@@ -1,8 +1,17 @@
-<header class = "toCenter">
-    <nav class="toCenter" style="background-color: #000000; width: 100%">
-        <div class="logo"></div>
-        <nav class = "setShadow ">
-            <ul class = "toCenter">
+<header>
+    <nav class="toCenter">
+        <nav class="container">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">
+                <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+            <a href="../index.php"><div class="logo"></div></a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            </svg>
+        </nav>
+        <nav class = " container">
+            <ul>
                 <li>Каталог</li>
                 <li>Каталог</li>
                 <li>Каталог</li>
