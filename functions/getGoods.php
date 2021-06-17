d3fd8118f0e4922a7b171c805b8a1d47f868d6fd
nickiway <nickiwayua@gmail.com>
2021-06-17 15:25:14 +0300

    Nick

--- functions/getGoods.php
+++ functions/getGoods.php
@@ -9,36 +9,22 @@ class GoodsCards{
         $this->sale = $sale;
         $this->stock = $stock;
     }
-    function cost(){
-        return $this->cost;
-    }
-    function name(){
-        return $this->name;
-    }
-    function image(){
-        return $this->image;
-    }
-    function sale(){
-        return $this->sale;
-    }
-    function stock(){
-        return $this->stock;
+    function is_sale (){
+        $percent = 0 ;
+        if($this->sale != 0){
+            $percent = round(100 - ($this->sale * 100 / $this->cost));
+            return array($percent,"block","line-through");
+        }
     }
 }
 $getItems = mysqli_query($connect, "SELECT * FROM goods");
 while($row = mysqli_fetch_assoc($getItems)){
     $class = new GoodsCards($row['Name'],$row['Cost'],$row['ImageLink'],$row['Sale'],$row['In stock']);
-    $percent = 0;
-    // Checking whethear we have sales
-    if ($row['Sale'] == 0) {
-        $displaySale = "none";
-        $textDecoration = "none";
-    } else {
-        $percent = round(100 - ($row['Sale'] * 100 / $row['Cost']));
-        $displaySale = "block";
-        $textDecoration = "line-through";
-    };
     // Checking whethear the items in stock
+    $textDecoration = "none";
+    $displaySale = "none";
+    $displaySale = $class->is_sale()[1];
+    $textDecoration = $class->is_sale()[2];
     if ($row['In stock'] == 1) {
     } else {
         continue;
@@ -46,11 +32,10 @@ while($row = mysqli_fetch_assoc($getItems)){
     // creating new cards
     echo 
     "<div class='cards'>
-        <div class = 'cards__close'>✖</div>
-        <div style = 'display : ".$displaySale."' class='cards__sale font-bold setShadow'>Скидка — ".$percent."%</div>
+        <div style = 'display : ".$displaySale."' class='cards__sale font-bold setShadow'>Скидка — ".$class->is_sale()[0]."%</div>
         <div class='cards__image'  style = 'background-image:url(images/".$row['ImageLink'].")'></div>
             <div class='cards__information'>
-                <div class='cards__name toCenter font-bold'>".$class->name()."</div>
+                <div class='cards__name toCenter font-bold'>".$class->name."</div>
                 <div class='cards__details'>
                     <div class='cards__description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, odio!</div>
                     <div class='toCenter'>
