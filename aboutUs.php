26537892ac86abbb72ba7e012c27711131ca5c19
Mike-Chaban <cmisa695@gmail.com>
2021-06-21 21:21:08 +0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Styles/main.css
    #	index.php
    #	siteItems/header.php

--- aboutUs.php
+++ aboutUs.php
@@ -26,90 +26,92 @@ require_once(ROOT.'\config\connect.php');
     <!-- Header -->
     <? require_once(ROOT.'\siteItems\header.php'); ?>
     <!-- Body -->
-    <h1 style="text-align: center; font-family: Impact">
-        Про нас
-    </h1>
-    <div class="box">
-        <div class="info_left">
-            <div>
-                <img src="images/Img1.jpg" alt="About Us" width="100%">
+    <div class="main_box">
+        <h1 style="text-align: center; font-family: Impact">
+            Про нас
+        </h1>
+        <div class="box">
+            <div class="info_left">
                 <div>
-                    <h2>Быстро работаем</h2>
-                    <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <img src="images/Img1.jpg" alt="About Us" width="100%">
+                    <div>
+                        <h2>Быстро работаем</h2>
+                        <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="images/Img1.jpg" alt="About Us" width="100%">
+                    <div>
+                        <h2>Быстро работаем</h2>
+                        <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="images/Img1.jpg" alt="About Us" width="100%">
+                    <div>
+                        <h2>Быстро работаем</h2>
+                        <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
                 </div>
             </div>
-            <div>
-                <img src="images/Img1.jpg" alt="About Us" width="100%">
-                <div>
-                    <h2>Быстро работаем</h2>
-                    <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div class="info_center">
+                <div class="punkt">
+                    1
+                </div>
+                <div  class="line">
+                </div>
+                <div class="punkt">
+                    2
+                </div>
+                <div  class="line">
+                </div>
+                <div class="punkt">
+                    3
+                </div>
+                <div  class="line">
+                </div>
+                <div class="punkt">
+                    4
+                </div>
+                <div  class="line">
+                </div>
+                <div class="punkt">
+                    5
+                </div>
+                <div  class="line">
+                </div>
+                <div class="punkt">
+                    6
                 </div>
             </div>
-            <div>
-                <img src="images/Img1.jpg" alt="About Us" width="100%">
+            <div class="info_right">
                 <div>
-                    <h2>Быстро работаем</h2>
-                    <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <div>
+                        <h2>Быстро работаем</h2>
+                        <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <img src="images/Img1.jpg" alt="About Us" width="100%">
+                </div>
+                <div>
+                    <div>
+                        <h2>Быстро работаем</h2>
+                        <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <img src="images/Img1.jpg" alt="About Us" width="100%">
+                </div>
+                <div>
+                    <div>
+                        <h2>Быстро работаем</h2>
+                        <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <img src="images/Img1.jpg" alt="About Us" width="100%">
                 </div>
             </div>
         </div>
-        <div class="info_center">
-            <div class="punkt">
-                1
-            </div>
-            <div  class="line">
-            </div>
-            <div class="punkt">
-                2
-            </div>
-            <div  class="line">
-            </div>
-            <div class="punkt">
-                3
-            </div>
-            <div  class="line">
-            </div>
-            <div class="punkt">
-                4
-            </div>
-            <div  class="line">
-            </div>
-            <div class="punkt">
-                5
-            </div>
-            <div  class="line">
-            </div>
-            <div class="punkt">
-                6
-            </div>
+        <h2 style="text-align: center">Где мы находимся</h2>
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1323.6729341030089!2d35.0555076349812!3d48.43070132179847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbfcc9cd38add9%3A0xec74071e4dd6c554!2sHoryana%20St%2C%2025%2C%20Dnipropetrovs&#39;k%2C%20Dnipropetrovs&#39;ka%20oblast%2C%2049000!5e0!3m2!1sen!2sua!4v1624214830990!5m2!1sen!2sua" loading="eager"></iframe>
         </div>
-        <div class="info_right">
-            <div>
-                <div>
-                    <h2>Быстро работаем</h2>
-                    <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                </div>
-                <img src="images/Img1.jpg" alt="About Us" width="100%">
-            </div>
-            <div>
-                <div>
-                    <h2>Быстро работаем</h2>
-                    <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                </div>
-                <img src="images/Img1.jpg" alt="About Us" width="100%">
-            </div>
-            <div>
-                <div>
-                    <h2>Быстро работаем</h2>
-                    <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                </div>
-                <img src="images/Img1.jpg" alt="About Us" width="100%">
-            </div>
-        </div>
-    </div>
-    <h2 style="text-align: center">Где мы находимся</h2>
-    <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1323.6729341030089!2d35.0555076349812!3d48.43070132179847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbfcc9cd38add9%3A0xec74071e4dd6c554!2sHoryana%20St%2C%2025%2C%20Dnipropetrovs&#39;k%2C%20Dnipropetrovs&#39;ka%20oblast%2C%2049000!5e0!3m2!1sen!2sua!4v1624214830990!5m2!1sen!2sua" loading="eager"></iframe>
     </div>
     <!-- Footer -->
     <? require_once(ROOT.'\siteItems\footer.php'); ?>
