433820174edb82282e45fb758a15dbbdb58f2a27
Mike-Chaban <cmisa695@gmail.com>
2021-08-02 01:02:28 +0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Styles/main.css
    #	index.php
    #	siteItems/header.php

--- siteItems/header.php
+++ siteItems/header.php
@@ -1,41 +1,54 @@
 <header>
     <link rel="stylesheet" href="../Bootstrap/css/fontawesome.min.css">
-    <div class="toCenter">
-        <div class="container">
-            <a href="../index.php"><div class="logo"></div></a>
+<!--    <div>-->
+<!--        <form  method = "get" class = "search">-->
+<!--            <input id="autoComplete" type="search" placeholder="Поиск..." dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off"  name = "Search" maxlength="2048" tabindex="1">-->
+<!--            <input type="submit" value="Search">-->
+<!--        </form>-->
+<!--        <button onclick = "window.location.href = 'index.php'" class = "search__cancel-button font-bold" style = "display:--><?php //echo $cancelButton; ?><!--">Clear All</button>-->
+<!--    </div>-->
+<!--    <img src="../icons/Корзина.png" height="31" alt="" onclick ="openCart();">-->
+<!--    <span id = "cartResult">0</span>-->
+<!--    <button onclick = "clearCart()">Clear Cart</button>-->
+    <nav>
+        <div class="cont_min">
+            <div class="contacts">
+                <div>
+                    +38 (099) 077 8596
+                </div>
+                <div>
+                    tchaban.aleksandr4002@gmail.com
+                </div>
+            </div>
+            <div class="networks">
+                <div><i class="fab fa-telegram-plane fa-MyX1"></i></div>
+                <div><i class="fab fa-viber fa-MyX1"></i></div>
+            </div>
         </div>
-    </div>
-    <div class="toCenter">
-        <nav class = "menu">
-            <div id="menu_btn">
-                <i class="fas fa-2x fa-bars" style="color: #fff"></i><br>
-                <span class="text">Меню</span>
+        <div class="cont_max">
+            <div>
+                <img src="../icons/Лого 1.png" alt="" width="350px">
             </div>
-<!--            <div>-->
-<!--                <form  method = "get" class = "search">-->
-<!--                    <input id="autoComplete" type="search" placeholder="Поиск..." dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off"  name = "Search" maxlength="2048" tabindex="1">-->
-<!--                    <input type="submit" value="Search">-->
-<!--                </form>-->
-<!--                <button onclick = "window.location.href = 'index.php'" class = "search__cancel-button font-bold" style = "display:--><?php //echo $cancelButton; ?><!--">Clear All</button>-->
-<!--            </div>-->
-            <div class="searcher">
-                <input type="text" autocomplete="off" placeholder="Поиск..." id="search">
-                <div class="magnifier"><i class="fas fa-MyX fa-reverse fa-search" style="color:#fff"></i></div>
-            </div>
-            <div class="cont_log_bask">
-                <div>
-                    <img src="../icons/Корзина.png" height="31" alt="" onclick ="openCart();">
-                    <span id = "cartResult">0</span>
-<!--                    <button onclick = "clearCart()">Clear Cart</button>-->
-                    <br>
-                    <span class="text">Корзина</span>
+            <div class="box">
+                <ul class="menu">
+                    <li>Каталог</li>
+                    <li>О компании</li>
+                    <li>Доставка и оплата</li>
+                    <li>Контакты</li>
+                </ul>
+                <div class="line"></div>
+                <div class="magnifier">
+                    <input type="text" class="search" placeholder="Поиск...">
+                    <i class="fas fa-search fa-MyX1 fa-reverse"></i>
                 </div>
-                <div>
-                    <img src="../icons/Login.png" width="32" alt=""><br>
-                    <span class="text">Войти</span>
+                <div class="login">
+                    <i class="fas fa-user-circle fa-2x"></i>
+                </div>
+                <div class="cart">
+                    <i class="fas fa-shopping-bag fa-2x"></i>
                 </div>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
 </header>
 <script src="JavaScript/cart.js"></script>
