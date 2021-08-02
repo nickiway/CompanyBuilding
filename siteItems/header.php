f036f928cb0d056c186be0c52afb439237407a5f
nickiway <nickiwayua@gmail.com>
2021-08-02 22:55:52 +0300

    Nick

--- siteItems/header.php
+++ siteItems/header.php
@@ -1,17 +1,17 @@
 <header>
     <link rel="stylesheet" href="../Bootstrap/css/fontawesome.min.css">
-<!--    <div>-->
-<!--        <form  method = "get" class = "search">-->
-<!--            <input id="autoComplete" type="search" placeholder="Поиск..." dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off"  name = "Search" maxlength="2048" tabindex="1">-->
-<!--            <input type="submit" value="Search">-->
-<!--        </form>-->
-<!--        <button onclick = "window.location.href = 'index.php'" class = "search__cancel-button font-bold" style = "display:--><?php //echo $cancelButton; ?><!--">Clear All</button>-->
-<!--    </div>-->
-<!--    <img src="../icons/Корзина.png" height="31" alt="" onclick ="openCart();">-->
-<!--    <span id = "cartResult">0</span>-->
-<!--    <button onclick = "clearCart()">Clear Cart</button>-->
+    <!-- <div>
+        <form  method = "get" class = "search">
+            <input id="autoComplete" type="search" placeholder="Поиск..." dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off"  name = "Search" maxlength="2048" tabindex="1">
+            <input type="submit" value="Search">
+        </form>
+        <button onclick = "window.location.href = 'index.php'" class = "search__cancel-button font-bold" style = "display:><?php echo $cancelButton; ?>">Clear All</button>
+    </div>
+    <img src="../icons/Корзина.png" height="31" alt="" onclick ="openCart();">
+    <span id = "cartResult">0</span>
+    <button onclick = "clearCart()">Clear Cart</button>  -->
     <nav>
-        <div class="cont_min">
+        <div class="extra-info">
             <div class="contacts">
                 <div>
                     +38 (099) 077 8596
@@ -25,21 +25,20 @@
                 <div><i class="fab fa-viber fa-MyX1"></i></div>
             </div>
         </div>
-        <div class="cont_max">
-            <div>
-                <img src="../icons/Лого 1.png" alt="" width="350px">
-            </div>
-            <div class="box">
+        <div class="main-menu">
+            <img src="../icons/Logo_1.png" alt="" width="300px">
+            <div class="toCenter">
                 <ul class="menu">
                     <li>Каталог</li>
                     <li>О компании</li>
                     <li>Доставка и оплата</li>
                     <li>Контакты</li>
                 </ul>
-                <div class="line"></div>
                 <div class="magnifier">
-                    <input type="text" class="search" placeholder="Поиск...">
-                    <i class="fas fa-search fa-MyX1 fa-reverse"></i>
+                    <form method = get class = "display-flex">
+                        <input type="text" class="search" name = "Search" placeholder="Поиск...">
+                        <i class="fas fa-search fa-MyX1 fa-reverse"></i>
+                    </form>
                 </div>
                 <div class="login">
                     <i class="fas fa-user-circle fa-2x"></i>
